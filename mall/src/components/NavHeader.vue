cf02b14759662654708dcaf0d462afcbd4d1d058
PFY1213 <1363872803@qq.com>
2020-09-04 00:36:58 +0800

    directory

---
+++ mall/src/components/NavHeader.vue
@@ -0,0 +1,12 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name: 'nav-header'
+}
+</script>
+<style scoped>
+
+</style>
---
+++ mall/src/pages/home.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <nav-header/>
+        <router-view/>
+        <nav-footer/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'nav-header'
+}
+</script>
+<style scoped>
+
+</style>
